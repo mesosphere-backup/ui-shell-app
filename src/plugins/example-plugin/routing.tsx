40c3578ab0b5f9624d51d9a74e40147967b64fe8
Dmitriy Rozhkov <drozhkov@mesosphere.io>
2019-02-11 13:42:22 +0100

    chore: rename extension

--- src/plugins/example-plugin/routing.tsx
+++ src/plugins/example-plugin/routing.tsx
@@ -4,7 +4,7 @@ import { injectable } from "inversify";
 import PageContent from "./components/PageContent";
 
 @injectable()
-export default class RepositoriesRouting {
+export default class ExamplePluginRouting {
   getRoutes(_partialNextState) {
     return [<Route path="/edge_lb/overview" component={PageContent} />];
   }
